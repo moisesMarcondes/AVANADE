a7f9e602540f96ab9a04aa519af369ba021ca0ab
moisesMarcondes <moises.marcondes@hotmail.com>
2022-05-28 11:59:54 -0300

    primeiro js

---
+++ primeiro.js
@@ -0,0 +1,36 @@
+// 1 - Crie uma função que retorne o seu nome
+
+function nome(Moisés) {
+   return "Moisés";
+  }
+  
+// 2 - Crie uma função que retorne o nome passado por parâmetro
+
+var nome1 = "Moisés"
+var nome2 = "gustava"
+
+  function nome(nome1, nome2) {
+    return "Moisés";
+   }
+
+
+
+// 3 - Faça uma função para dividir 2 números
+   function divisao() {
+    return 15 / 18;
+   }
+console.log (divisao())
+
+
+// 4 faça uma função que retorne um array com o nome dos alunos presentes na aula
+   var nomesDosAlunos = ["Cassia","Felipe","Nilce","Erick","Moisés","André"];
+   
+   function Aula(nomeDosAluno)
+   {
+           return nomeDosAluno
+   
+     }
+
+     console.log (nomesDosAlunos)
+
+     // 5 - faça uma função que recebe um array e um valor, adicione o valor no array e retorne o array
